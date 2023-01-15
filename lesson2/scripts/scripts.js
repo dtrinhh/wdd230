1b5a8de50d98c086fd08755143b73c9d52a95ee2
dtrinhh <dustintrinhh@gmail.com>
2023-01-14 23:57:28 -0700

    Design Principles Documents

---
+++ lesson2/scripts/scripts.js
@@ -0,0 +1,5 @@
+const copyright = document.querySelector('#copyright');
+
+copyright.textContent = `© ${new Date().getFullYear()}`;
+
+let oLastModif = document.getElementById("lastupdated").innerText = document.lastModified;
